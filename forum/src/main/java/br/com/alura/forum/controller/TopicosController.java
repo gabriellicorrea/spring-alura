6214ad0b4a539925b7a2c5269475dd0515678bda
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-04-22 20:25:33 -0300

    tratando excessoes

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,7 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
-@RequestMapping("/topico")
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
@@ -40,7 +42,7 @@ public class TopicosController {
 	}	
 	
 	@PostMapping 
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 	
--- forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,13 +1,29 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sun.istack.NotNull;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	public String titulo;
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
 	public String mensagem;
+	
+	@NotNull
+	@NotEmpty
 	public String nomeCurso;
 
 
