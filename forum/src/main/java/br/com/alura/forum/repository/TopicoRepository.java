48927bd83871cdb4b17833eed105acb58f31cd25
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-04-22 15:52:09 -0300

    adicionando os metodos findAll e FindBy

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,18 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+
+	//@Query("SELECT t FROM topico t WHERE t.cuso.nome = :nomeCurso")
+	//List<Topico> carregarPorNomeDoCurso(@Param("nomeCuso")String nomeCurso);
 
 }
