8eb5e702864bb6156194302182f5d3ffe029a6d3
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-04-23 10:21:44 -0300

    adicionando o metodo para listar

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -3,8 +3,7 @@ package br.com.alura.forum.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
+
 
 import br.com.alura.forum.modelo.Topico;
 
