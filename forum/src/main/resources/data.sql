095f186a8c6b86a0d351d4f29b07507aea27d53e
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-04-23 19:39:44 -0300

    autorizando usuarios

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$rvFm7aztokzN0Wm1wFREOOPRhadZ.HECRofB5/0O2LeU3ELRVEFlm');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
