e4aa35afc66ab7403cc8f2f81b3d2bb80642df81
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-04-28 10:41:01 -0300

    testes no repository

---
+++ forum/src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.forum.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutenticacaoControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
